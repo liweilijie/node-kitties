00dea888e20462569ec18fbdb52513951402d860
liwei <liweilijie@gmail.com>
2022-01-02 22:26:13 +0800

    add kitties.

---
+++ pallets/pallet-kitties/src/lib.rs
@@ -0,0 +1,64 @@
+#![cfg_attr(not(feature = "std"), no_std)]
+
+pub use pallet::*;
+
+#[frame_support::pallet]
+pub mod pallet {
+	use frame_support::{
+		dispatch::{DispatchResult, DispatchResultWithPostInfo},
+		pallet_prelude::*,
+		sp_runtime::traits::{Hash, Zero},
+		traits::{Currency, ExistenceRequirement, Randomness},
+	};
+	use frame_system::pallet_prelude::*;
+	use sp_io::hashing::blake2_128;
+	use codec::{Encode, Decode};
+
+
+	#[pallet::pallet]
+	#[pallet::generate_store(pub(super) trait Store)]
+	pub struct Pallet<T>(_);
+
+	#[derive(Encode, Decode)]
+	pub struct Kitty(pub [u8;16]);
+	type KittyIndex = u32;
+
+	/// Configure the pallet by specifying the parameters and types it depends on.
+	#[pallet::config]
+	pub trait Config: frame_system::Config {
+		type Event: From<Event<Self>> + IsType<<Self as frame_system::Config>::Event>;
+
+		type Currency: Currency<Self::AccountId>;
+	}
+
+	// Errors.
+	#[pallet::error]
+	pub enum Error<T> {
+	}
+
+	#[pallet::event]
+	#[pallet::generate_deposit(pub(super) fn deposit_event)]
+	pub enum Event<T: Config> {
+	}
+
+	#[pallet::storage]
+	#[pallet::getter(fn kitties_count)]
+	pub type KittiesCount<T> = StorageValue<_, u32>;
+
+	#[pallet::storage]
+	#[pallet::getter(fn kitties)]
+	pub type Kitties<T> = StorageMap<_, Blake2_128Concat, KittyIndex, Option<Kitty>, ValueQuery>;
+
+	#[pallet::storage]
+	#[pallet::getter(fn owner)]
+	pub type Owner<T: Config> = StorageMap<_, Blake2_128Concat, KittyIndex, Option<T::AccountId>, ValueQuery>;
+
+
+	#[pallet::call]
+	impl<T: Config> Pallet<T> {
+	}
+
+
+	impl<T: Config> Pallet<T> {
+	}
+}
